ba0c0a40528fff628dd0df2131bfc6889817098e
serpentz <abyicherinet@gmail.com>
2020-03-29 21:55:31 -0400

    purge graphql 10, downgrade to 8

--- app/graphql/types/user_type.rb
+++
@@ -1,9 +0,0 @@
-module Types
-  class UserType < Types::BaseObject
-    field :id, ID, null: false
-    field :username, String, null: false
-    field :first_name, String, null: false
-    field :last_name, String, null: false
-    field :password, String, null: false
-  end
-end
